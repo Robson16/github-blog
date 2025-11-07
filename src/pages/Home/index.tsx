843f3327881658e9404edb82f5275c383118a253
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-11-06 22:15:39 -0300

    feat: displaying a list of issues

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,43 @@
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { useContextSelector } from 'use-context-selector'
 import { Profile } from '../../components/Profile'
-import { HomeContainer } from './styles'
+import { GithubContext } from '../../contexts/GithubContext'
+import { HomeContainer, IssueItem, IssueList } from './styles'
 
 export function Home() {
+  const issues = useContextSelector(GithubContext, (context) => {
+    return context.issues
+  })
+
+  const issuesTotalPages = useContextSelector(GithubContext, (context) => {
+    return context.issuesTotalPages
+  })
+
   return (
     <HomeContainer>
       <Profile />
+
+      <IssueList>
+        {issues.map((issue) => {
+          return (
+            <IssueItem key={issue.id}>
+              <header>
+                <h1>{issue.title.slice(0, 46) + '...'}</h1>
+                <span>
+                  {formatDistanceToNow(new Date(issue.created_at), {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </span>
+              </header>
+              <p>{issue.body.slice(0, 150) + '...'}</p>
+            </IssueItem>
+          )
+        })}
+      </IssueList>
+
+      <p>Total de páginas: {issuesTotalPages}</p>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -10,3 +10,50 @@ export const HomeContainer = styled.main`
   padding-top: 3.5rem;
   padding-bottom: 3.5rem;
 `
+
+export const IssueList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  gap: 2rem;
+  width: 100%;
+  margin: 3rem 0;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    gap: 1rem;
+    padding: 0 1rem;
+  }
+`
+
+export const IssueItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+  padding: 2rem;
+  border-radius: 10px;
+  background-color: ${(props) => props.theme.colors['base-post']};
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    gap: 1rem;
+    overflow-wrap: break-word;
+
+    h1 {
+      font-size: 1.25rem;
+      color: ${(props) => props.theme.colors['base-title']};
+    }
+
+    span {
+      font-size: 0.875rem;
+      color: ${(props) => props.theme.colors['base-span']};
+      white-space: nowrap;
+    }
+  }
+
+  p {
+    color: ${(props) => props.theme.colors['base-text']};
+    overflow-wrap: break-word;
+  }
+`
